77d3c5f75ecfa35e26fa59b0abf3e50614393a4e
Carlos Mora <carlos.mora.ram@gmail.com>
2026-02-04 22:21:52 -0500

    cambio tamaño logo md

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,7 +57,7 @@ export const Navbar = () => {
               alt="Carlos Mora"
               width={100}
               height={100}
-              className="w-20 md:w-30 h-auto"
+              className="w-20 md:w-[100px] h-auto"
             />
           </Link>
 
